45f0904f839a4a8cd03d3d711e1d6ad77c3c4320
unknown <nick_clymer2018@yahoo.com>
2021-07-05 20:10:50 -0600

    added error and success handlers with notation

--- public/database.js
+++ public/database.js
@@ -4,3 +4,26 @@ const indexedDB =
     window.webkitIndexedDB ||
     window.msIndexedDB ||
     window.shimIndexedDB;
+
+let db;
+const request = indexedDB.open("budget, 1");
+
+// db version control and update
+request.onupgradeneeded = ({ target }) => {
+    let db = target.result;
+    db.createdObjectStore('pending', { autoIncrement: true });
+};
+
+// db submission success logic
+request.onsuccess = ({ target }) => {
+    db = target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }    
+};
+
+// error user return message
+request.onerror = function (event) {
+    console.log("Hmmm something went wrong! " + event.target.errorCode);
+};
